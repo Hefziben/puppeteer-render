9829ab2f7c767a98769e89dee0af84a09f6e43c4
Milton Arenas <hefziben@gmail.com>
2025-01-16 10:17:22 -0500

    basic setup

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
 const messageRouter = require("./routes/messages");
 const connectDB = require("./database/index");
+// Initialize the Convex client with your deployment URL
 
-connectDB();
+// const { ConvexHttpClient } = require("convex/browser");
+// const convex = new ConvexHttpClient('https://capable-toad-329.convex.cloud');
+// const {api} = require("./convex/_generated/api.js")
+
+//connectDB();
+
+// async function getMessages() {
+//   convex.query(api.functions.getItems).then(console.log);
+
+// }
+
+//getMessages()
 console.log('Node.js version:', process.version);
 const PORT = process.env.PORT || 4000;
 app.use("/api/messages", messageRouter);
