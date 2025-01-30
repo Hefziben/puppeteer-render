304f370a748c9ae7a6a2e925996a3a2738888a46
Milton Arenas <hefziben@gmail.com>
2025-01-30 18:02:33 -0500

    added morgan for log

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan"); // Import morgan
 const app = express();
 
 require("dotenv").config();
@@ -6,13 +7,15 @@ const messageRoutes = require("./routes/messages");
 
 const PORT = process.env.PORT || 4000;
 
+app.use(morgan("tiny")); // Logs requests in a short format
 
 app.get("/", (req, res) => {
   res.send("Render Puppeteer server is up and running!");
 });
 
 app.get("/ping", (req, res) => {
-  res.send("I was pinged by Server");
+  res.status(200).json({message:'I was pinned by Server'});
+  
 });
 
 app.use("/api/messages", messageRoutes)
